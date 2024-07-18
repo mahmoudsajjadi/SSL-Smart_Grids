eeb487e1a56dd1e1417e1f6d902b95c176a13be3
Mahmoud Sajjadi <mahmoud.sajjadi@gmail.com>
2024-07-17 17:57:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+from torch import nn
+
+from lightly.loss import NTXentLoss
+from lightly.models.modules import SimCLRProjectionHead
+from lightly.transforms.simclr_transform import SimCLRTransform
